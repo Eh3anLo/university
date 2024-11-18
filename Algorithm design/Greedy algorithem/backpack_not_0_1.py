01d624943b93eff0d3e68cb27a1d223ffb346712
ehsan <ehsunlotfy@gmail.com>
2024-11-18 21:59:20 +0330

    add example on comment

--- Algorithm design/Greedy algorithem/backpack_not_0_1.py
+++ Algorithm design/Greedy algorithem/backpack_not_0_1.py
@@ -1,8 +1,3 @@
-prices = list(map(int,input("Enter prices of objects (ex:1,23,45,6): ").split(',')))
-weights = list(map(int,input("Enter weights of objects (ex:1,23,45): ").split(',')))
-backpack_weight = int(input("Backpack weight: "))
-solution = [0 for i in range(len(prices))]   
-
 def pw_sort(prices,weights):
     data_pack = []
     for i in range(0, len(prices)):
@@ -31,8 +26,16 @@ def knapsack():
         i+=1
     return max_price , data_pack
 
+prices = list(map(int,input("Enter prices of objects (ex:1,23,45,6): ").split(','))) # -> 15,24,25
+weights = list(map(int,input("Enter weights of objects (ex:1,23,45): ").split(','))) # -> 10,15,18
+backpack_weight = int(input("Backpack weight: ")) # -> 20
+solution = [0 for i in range(len(prices))]   
+
 max_price , data_pack = knapsack()
-print(f"{max_price=}")
-print(f"{data_pack=}")
-print("output : " , solution)
+print(f"{max_price=}") ## max_price=31.5
+print(f"{data_pack=}") ## data_pack=[(24, 15, 1.6), (15, 10, 1.5), (25, 18, 1.4)]
+print("output : " , solution) ## [1, 0.5, 0]
+
+
+
 
